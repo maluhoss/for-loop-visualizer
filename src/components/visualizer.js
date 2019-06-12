2129a538dd9c20cf5a02a5265d30d730779ca47b
Maliha Hossain <malu.hoss@gmail.com>
2019-06-12 16:02:57 -0400

    displayed for loop on webpage

--- src/components/visualizer.js
+++ src/components/visualizer.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 export default class Visualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startingPoint: 0,
+      inputArray: [1, 2, 3],
+      direction: ['<','++'],
+      functionBody: "{ return inputArray[i]; }"
+    }
+  }
+
   render() {
     return (
-      <p>I am the visualizer</p>
+      <div>
+        <p>for (let i = {this.state.startingPoint}; i {this.state.direction[0]} inputArray.length; i{this.state.direction[1]}) {this.state.functionBody}
+        </p>
+      </div>
     );
   }
 }
