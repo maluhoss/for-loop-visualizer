986ccfc56f4000a6246834197004f6e8d22727e7
Maliha Hossain <malu.hoss@gmail.com>
2019-07-18 22:16:48 -0400

    took out backend as was not required for this static web app

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
-let express = require("express");
-let app = express();
-let PORT = 8080;
+// let express = require("express");
+// let app = express();
+// let PORT = 8080;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}!`);
+// });
 
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get('/', (req,res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
